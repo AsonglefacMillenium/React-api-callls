876e8882efe5974a8349b3afe9d390b8c336ecd8
Asonglefac Millenium <asonglefacmillenium@gmail.com>
2021-12-05 05:24:40 +0100

    adding search fetching and loading

--- src/Axios/Main.js
+++ src/Axios/Main.js
@@ -1,40 +1,59 @@
-import React, {useState, useEffect} from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import axios from 'axios';
 
 const Main = () => {
 
-const [data, setdata] = useState({hits: []});
+    const [data, setdata] = useState({ hits: [] });
+    const [query, setQuery] = useState('redux');
+    const [url, setUrl] = useState('https://hn.algolia.com/api/v1/search?query=redux');
+    const [isLoading, setIsLoading] = useState(false);
 
-useEffect(() => {
-    const fetchData = async () =>{
-        const result = await axios(
-            'https://hn.algolia.com/api/v1/search?query=redux'
-    
-        );
-    
-        setdata(result.data);
-        
-    }
-    fetchData();
-   
-   
-}, [])
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            const result = await axios(url);
+
+            setdata(result.data);
+            setIsLoading(false);
+
+        }
+        fetchData();
+
+
+    }, [url])
 
 
     return (
-        <div className="main">
-          <ul>
-              {data.hits.map(item => (
-                  <li key={item.ObjectID}>
-                      <a href={item.url}> {item.title}</a>
-                  </li>
-              )
+        <Fragment>
+            <input type="text"
+                value={query}
+                onChange={e => setQuery(e.target.value)}
+            />
 
-              )}
-          </ul>
-        </div>
+            <button type="button" 
+            onClick={() => setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)}>Search</button>
+
+
+{
+    isLoading ? (
+        <div>Loading.....</div>
+    ) : (
+        <ul>
+                {data.hits.map(item => (
+                    <li key={item.ObjectID}>
+                        <a href={item.url}> {item.title}</a>
+                    </li>
+                )
+
+                )}
+            </ul>
+    )
+}
+           
+
+        </Fragment>
+
     )
 }
 
 export default Main
- 
